1316ac5f0f0432811aa4caf6b109b8913e6a72ac
NeoGlanding <neoglanding@protonmail.com>
2021-08-07 22:24:05 +0700

    ubah huruf ke kapital

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function renderHTML(data) {
-    const html = `<h1>${data.name}</h1>
-    <h1>${data.bio}</h1>
-    <img src="${data.avatar_url}" alt="profile.jpg">
-    <h1>Following ${data.following}</h1>
-    <h1>Follower ${data.followers}</h1>
-    `
-    return html;
-} 
-
-function fetchApi() {
-    fetch('https://api.github.com/users/NeoGlanding')
-     .then(response => response.json())
-     .then(data => {
-         console.log(data)
-         document.getElementById('github-profiles').insertAdjacentHTML('afterbegin', renderHTML(data))
-        })
-     .catch(err => console.log(err))
+function wordChanger(word) {
+    let str = ''
+    for (let i = 0;i<word.length;i++) {
+        if (i === 0 || word[i-1] === ' ') {
+            str += word[i].toUpperCase()
+        } else {
+            str += word[i]
+        }
+    }
+    console.log(str)
 }
 
-fetchApi()
+wordChanger('Selamat Pagi')
