c6f3829d9dd786374d72350e7980772064220af3
NeoGlanding <neoglanding@protonmail.com>
2021-08-07 22:08:08 +0700

    dom

--- script.js
+++ script.js
@@ -1,68 +1,21 @@
-function ekstrakurikuler(data){
-    //your code here!
-    let obj = {};
-    for (let i = 0;i<data.length;i++) {
-        let name = data[i].name;
-        for (let j = 0;j<data[i].club.length;j++) {
-            let groupName = data[i].club[j];
-            if (!obj[groupName]) {
-                obj[groupName] = {}
-            }
-            let kelas = data[i].class;
-            if (!obj[groupName][kelas]) {
-                obj[groupName][kelas] = [];
-            }
-            obj[groupName][kelas].push(name)
-        }
-    }
-    return obj;
-  }
-  
-  const data = 
-  [
-    {
-      name: 'Adi',
-      class: 'Mawar',
-      club: ['Bola', 'Bulutangkis']
-    },
-    {
-      name: 'Cinthya',
-      class: 'Melati',
-      club: ['Membaca', 'Bulutangkis']
-    },
-    {
-      name: 'Andika',
-      class: 'Mawar',
-      club: ['Bulutangkis', 'Membaca']
-    },
-    {
-      name: 'Daniel',
-      class: 'Mawar',
-      club: ['Bola', 'Renang']
-    }
-  ]
+function renderHTML(data) {
+    const html = `<h1>${data.name}</h1>
+    <h1>${data.bio}</h1>
+    <img src="${data.avatar_url}" alt="profile.jpg">
+    <h1>Following ${data.following}</h1>
+    <h1>Follower ${data.followers}</h1>
+    `
+    return html;
+} 
 
-let obj1 = {
-    nama: 'Gilang'
+function fetchApi() {
+    fetch('https://api.github.com/users/NeoGlanding')
+     .then(response => response.json())
+     .then(data => {
+         console.log(data)
+         document.getElementById('github-profiles').insertAdjacentHTML('afterbegin', renderHTML(data))
+        })
+     .catch(err => console.log(err))
 }
 
-console.log(ekstrakurikuler(data));
-// console.log(data.club)
-
-/* example output:
-{
-  Bola: {
-    Mawar: ['Adi', 'Daniel']
-  },
-  Bulutangkis: {
-    Mawar: ['Adi', 'Andika'],
-    Melati: ['Cinthya']
-  },
-  Membaca: {
-    Melati: ['Cinthya'],
-    Mawar: ['Andika']
-  },
-  Renang: {
-    Mawar: ['Daniel']
-  }
-} */
+fetchApi()
